11ee43f21bb45b12ba002a320506c1f770cd61c0
David Ramirez <david@MacBook-Pro-5.local>
2018-08-16 10:24:00 -0700

    created stylesheet for posts

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
 				 */
+
 				get_template_part( 'src/templates/content', get_post_type() );
 
 			endwhile;
