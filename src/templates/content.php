630efed4dbb7239a60e4372787c80b1f49277266
David Ramirez <david@MacBook-Pro-5.local>
2018-08-16 15:45:49 -0700

    pralax effect added

--- src/templates/content.php
+++ src/templates/content.php
@@ -10,46 +10,50 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('post-section'); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
+	<div class="clipping">
+		<div class="post-content">
+			<header class="entry-header">
 				<?php
-				// presspack_posted_on();
-				// presspack_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+				if ( is_singular() ) :
+					the_title( '<h1 class="entry-title">', '</h1>' );
+				else :
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				endif;
 
-	<?php //vold_post_thumbnail(); ?>
+				if ( 'post' === get_post_type() ) :
+					?>
+					<div class="entry-meta">
+						<?php
+						// presspack_posted_on();
+						// presspack_posted_by();
+						?>
+					</div><!-- .entry-meta -->
+				<?php endif; ?>
+			</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'vold' ),
-				array(
-					'span' => array(
-						'class' => array(),
+			<?php //vold_post_thumbnail(); ?>
+
+			<div class="entry-content">
+				<?php
+				the_content( sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'vold' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
 					),
-				)
-			),
-			get_the_title()
-		) );
+					get_the_title()
+				) );
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'vold' ),
-			'after'  => '</div>',
-		) );
-		?>
-	</div><!-- .entry-content -->
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'vold' ),
+					'after'  => '</div>',
+				) );
+				?>
+			</div><!-- .entry-content -->
+		</div>
+	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
